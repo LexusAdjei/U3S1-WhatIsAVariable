b3c749fb3d4de757118451b24849f11a1006264b
Lexus Adjei <lexus.adjei@cdpipelinedevshop.com>
2025-01-06 17:00:37 -0500

    Update
    Updated thing

--- src/main/java/com/codedifferently/lab/partA/Variables02.java
+++ src/main/java/com/codedifferently/lab/partA/Variables02.java
@@ -1,16 +1,17 @@
-package com.codedifferently.lab.partA;
+//package com.codedifferently.lab.partA;
 
-public class Variables02 {
-    public static void main(String[] args) {
+//public class Variables02 {
+    //public static void main(String[] args) {
         //RUN THE PROGRAM BETWEEN EACH STEP AND OBSERVE THE OUTPUT
         //Step 1) Change the print statements in lines 9 and 13 so that they print the value of x.
         //Step 2) Change the value of x to "kitty".
-        String x = "puppy";
-        System.out.println("puppy");
+        //String x = "kitty";
+        //System.out.println(x);
 
         //Step 3) Add a line of code that changes the value of x to "bunny".
+        //x = "bunny";
 
-        System.out.println("puppy");
+        //System.out.println(x);
 
-    }
-}
+    //}
+//}
--- src/main/java/com/codedifferently/lab/partB/Main.java
+++ src/main/java/com/codedifferently/lab/partB/Main.java
@@ -1,4 +1,14 @@
 package com.codedifferently.lab.partB;
 
 public class Main {
+    public static void main(String[] args) {
+        Coin coin1 = new Coin();
+        Coin coin2 = new Coin("Quarter", .25);
+        Coin coin3 = new Coin("Dime", .10);
+        Coin coin4 = new Coin("Penny", .01);
+        System.out.println(coin1);
+        System.out.println(coin2);
+        System.out.println(coin3);
+        System.out.println(coin4);
+    }
 }
